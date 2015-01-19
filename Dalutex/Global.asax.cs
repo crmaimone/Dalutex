64bdeb3a98ced5fd31212be81dd3b364a6b29569
odagovoni <odagovoni@gmail.com>
2015-01-19 10:21:58 -0200

    oda

--- Dalutex/Global.asax.cs
+++ Dalutex/Global.asax.cs
@@ -12,6 +12,7 @@ namespace Dalutex
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
     //teste
+    //teste oda -----
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()

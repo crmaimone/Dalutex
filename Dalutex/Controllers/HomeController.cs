c4d3d4f0b2cdcfe17b4de22f5591acae920561c0
crmaimone <crmaimone@gmail.com>
2015-01-19 11:34:02 -0200

    Contato

--- Dalutex/Controllers/HomeController.cs
+++ Dalutex/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace Dalutex.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Sobre a Dalutex:";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Entre com contato conosco:";
 
             return View();
         }

39b7e340988d4552fd6c55a695ab88132b034935
odagovoni <odagovoni@gmail.com>
2015-01-26 17:58:03 -0200

    commit só pra não esquecer

--- Dalutex/Models/AccountViewModels.cs
+++ Dalutex/Models/AccountViewModels.cs
@@ -13,7 +13,7 @@ namespace Dalutex.Models
         [Display(Name = "Senha")]
         public string Password { get; set; }
 
-        [Display(Name = "Lembre-se de mim")]
+        [Display(Name = "Lembre-se de mim!")]
         public bool RememberMe { get; set; }
     }
 

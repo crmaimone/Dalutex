30f2d87441ef83681f318e2499b433272c6f460c
odagovoni <odagovoni@gmail.com>
2015-01-26 08:35:47 -0200

    views

---
+++ Dalutex/Models/DataModels/VW_CARACT_DESENHOS.cs
@@ -0,0 +1,23 @@
+namespace Dalutex.Models.DataModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("TIDALUTEX.VW_CARACT_DESENHOS")]
+    public partial class VW_CARACT_DESENHOS
+    {
+        //[Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+
+        [StringLength(4)]
+        public string DESENHO { get; set; }
+        public int? ID_CARAC_TEC { get; set; }
+        public string CARACT_TECNICA { get; set; }
+        public int? ID_TECNOLOGIA { get; set; }
+        public string TECNOLOGIA { get; set; }
+
+    }
+}

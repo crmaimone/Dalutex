eaf4fe253f438253152d65bbcfa5b40d3ccf4887
crmaimone <crmaimone@gmail.com>
2015-12-10 21:59:55 -0200

    TESTE DE COMMIT

--- Dalutex/Global.asax.cs
+++ Dalutex/Global.asax.cs
@@ -19,6 +19,7 @@ namespace Dalutex
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //TESTE
         }
     }
 }

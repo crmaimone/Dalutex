6e8c260e8f8b127b5ccb1e35154dc0d47d57fba0
crmaimone <crmaimone@gmail.com>
2015-12-12 19:27:17 -0200

    Marco nas modificações de layout do espelho

--- Dalutex/Global.asax.cs
+++ Dalutex/Global.asax.cs
@@ -19,7 +19,6 @@ namespace Dalutex
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            //TESTE
         }
     }
 }
